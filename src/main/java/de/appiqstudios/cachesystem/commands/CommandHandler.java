08ebae3d4487ec75a65ea7bb2f303a0f8863e798
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2025-09-09 16:37:41 +0200

    chore: registered new builtin commands

--- src/main/java/de/appiqstudios/cachesystem/commands/CommandHandler.java
+++ src/main/java/de/appiqstudios/cachesystem/commands/CommandHandler.java
@@ -1,9 +1,14 @@
 package de.appiqstudios.cachesystem.commands;
 
 
-import de.appiqstudios.cachesystem.core.Server;
+import de.appiqstudios.cachesystem.commands.builtin.GetCommand;
+import de.appiqstudios.cachesystem.commands.builtin.InfoCommand;
+import de.appiqstudios.cachesystem.commands.builtin.SetCommand;
+import de.appiqstudios.cachesystem.core.builtin.ClientHandler;
+import de.appiqstudios.cachesystem.core.builtin.Server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,9 +16,16 @@ import java.util.List;
 public class CommandHandler {
 
     private final List<Command> availableCommands = new ArrayList<>();
+    private final PrintWriter out;
+    private final ClientHandler handler;
 
-    public CommandHandler(Server server, Socket socket) throws IOException {
-        availableCommands.add(new InfoCommand(server, socket));
+    public CommandHandler(Server server, Socket socket, PrintWriter out, ClientHandler handler) throws IOException {
+        this.out = out;
+        this.handler = handler;
+
+        availableCommands.add(new InfoCommand(server, socket, out));
+        availableCommands.add(new SetCommand(server, socket, out, handler.getStorage()));
+        availableCommands.add(new GetCommand(server, socket, out, handler.getStorage()));
     }
 
     public void checkCommand(String input) {
@@ -29,7 +41,7 @@ public class CommandHandler {
             }
         }
 
-        System.out.println("Unknown command: " + commandName);
+        out.println("Unknown command: " + commandName);
     }
 
 }
