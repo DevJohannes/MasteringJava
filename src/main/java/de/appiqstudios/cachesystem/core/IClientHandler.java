caa622740cb8ba3f94e21fe092647a8512d0a90d
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2025-09-09 16:39:00 +0200

    feat: added storage to client handler

--- src/main/java/de/appiqstudios/cachesystem/core/IClientHandler.java
+++ src/main/java/de/appiqstudios/cachesystem/core/IClientHandler.java
@@ -1,16 +1,25 @@
 package de.appiqstudios.cachesystem.core;
 
+import de.appiqstudios.cachesystem.cache.IStorage;
+
 import java.net.Socket;
 import java.util.UUID;
+
+/**
+ * Represents a client handler that manages communication with a connected client.
+ * This class implements Runnable to allow handling client connections in separate threads.
+ **/
 
 public abstract class IClientHandler implements Runnable {
 
     private Socket socket;
     private UUID uuid;
+    private IStorage storage;
 
-    public IClientHandler(Socket socket, UUID uuid) {
+    public IClientHandler(Socket socket, UUID uuid, IStorage storage) {
         this.socket = socket;
         this.uuid = uuid;
+        this.storage = storage;
     }
 
     @Override
