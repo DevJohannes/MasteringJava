e6a9459e8dc9ab1ea3e5c595a941b4cfac3b4d66
Creative_Dreamer <97633322+DevJohannes@users.noreply.github.com>
2025-09-10 12:56:34 +0200

    chore: put implemented server class in builtin

--- src/main/java/de/appiqstudios/cachesystem/core/builtin/Server.java
+++ src/main/java/de/appiqstudios/cachesystem/core/builtin/Server.java
@@ -1,15 +1,17 @@
-package de.appiqstudios.cachesystem.core;
+package de.appiqstudios.cachesystem.core.builtin;
+
+import de.appiqstudios.cachesystem.cache.builtin.Storage;
+import de.appiqstudios.cachesystem.core.IServer;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 /*
 * The Server class sets up a server socket to listen for incoming client connections.
-* For each connected client, it starts a new ClientHandler thread to manage communication.
+* For each connected client, it starts a new IClientHandler thread to manage communication.
 * It implements the IServer interface to provide the functionality of the server.
 */
 public class Server implements IServer {
@@ -35,7 +37,7 @@ public class Server implements IServer {
                 System.out.println("Neuer Client verbunden: " + clientSocket.getInetAddress().getHostAddress() + " mit ID: " + clientId);
 
                 // Starting a new thread for each connected client after their connection is accepted
-                new Thread(new ClientHandler(this, clientSocket, clientId)).start();
+                new Thread(new ClientHandler(this, clientSocket, clientId, new Storage())).start();
             }
         }  catch (IOException e) {
             throw new RuntimeException(e);
